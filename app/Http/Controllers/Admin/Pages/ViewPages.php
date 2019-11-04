e149f93304c10e99bd0790a8695eb940f5b9f975
Christopher Pitt <cgpitt@gmail.com>
2019-11-04 21:46:59 +0200

    Initial commit

---
+++ app/Http/Controllers/Admin/Pages/ViewPages.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Pages;
+
+class ViewPages {
+    public function handle()
+    {
+        return view('admin.pages.view-pages');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix("/admin")->namespace("Admin\\")->group(function() {
+    Route::prefix("/pages")->namespace("Pages\\")->group(function() {
+        Route::get("/", "ViewPages@handle");
+    });
+});
