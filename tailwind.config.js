94249e2e2cb73bf346c98040975a56b51d64df3b
Christopher Pitt <cgpitt@gmail.com>
2019-12-16 23:08:32 +0200

    Add SVG to purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
         extend: {},
     },
     variants: {
-        display: ["responsive", "hover", "focus", "active", "group-hover"],
-        opacity: ["responsive", "hover", "focus", "active", "group-hover"],
-        borderWidth: ["responsive", "hover", "focus", "active", "group-hover"],
-        borderColor: ["responsive", "hover", "focus", "active", "group-hover"],
-        height: ["responsive", "hover", "focus", "active", "group-hover"],
-        padding: ["responsive", "hover", "focus", "active", "group-hover"],
+        display: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+        opacity: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+        borderWidth: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+        borderColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+        height: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+        padding: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
     },
     plugins: [],
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,16 @@
-const mix = require("laravel-mix")
+const mix = require('laravel-mix')
 
-require("laravel-mix-tailwind")
-require("laravel-mix-purgecss")
+require('laravel-mix-tailwind')
+require('laravel-mix-purgecss')
 
-mix.js("resources/js/app.js", "public/js")
-    .postCss("resources/css/highlight.pcss", "public/css")
-    .postCss("resources/css/app.pcss", "public/css")
-    .tailwind("./tailwind.config.js")
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/highlight.pcss', 'public/css')
+    .postCss('resources/css/app.pcss', 'public/css')
+    .tailwind('./tailwind.config.js')
 
 if (mix.inProduction()) {
     mix.version().purgeCss({
         whitelistPatterns: [/language/, /hljs/],
+        extensions: ['html', 'js', 'php', 'svg'],
     })
 }
