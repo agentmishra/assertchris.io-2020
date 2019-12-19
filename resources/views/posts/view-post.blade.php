9d8ad2273297677bc77a95ed73e1d62c35a70d1d
Christopher Pitt <cgpitt@gmail.com>
2019-12-19 18:01:05 +0200

    Add some css improvements

--- resources/views/posts/view-post.blade.php
+++ resources/views/posts/view-post.blade.php
@@ -46,7 +46,7 @@
             I write about all sorts of interesting code things, and I'd love to share them with you. I will only
             send you updates from the blog, and <strong class="font-medium">will not share your email address with anyone</strong>.
         </p>
-        <form action="https://assertchris.us12.list-manage.com/subscribe/post?u=af7934ca8606594b204011bcc&amp;id=a8e81e08e9" method="post" target="_blank" class="flex flex-row w-full md:w-2/3">
+        <form action="https://assertchris.us12.list-manage.com/subscribe/post?u=af7934ca8606594b204011bcc&amp;id=a8e81e08e9" method="post" target="_blank" class="flex flex-row flex-wrap w-full md:w-2/3">
             <input type="email" value="" name="EMAIL" class="flex flex-grow text-gray-900 border-0 border-b-2 text-base border-gray-500 focus:border-blue-300 outline-none mr-2 px-3 py-2">
             <input type="text" name="b_af7934ca8606594b204011bcc_a8e81e08e9" tabindex="-1" value="" style="position: absolute; left: -5000px;" aria-hidden="true">
             <input type="submit" value="Subscribe" name="subscribe" class="px-4 py-2 bg-blue-400 text-white border-0 text-base">
