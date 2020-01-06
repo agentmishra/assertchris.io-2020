d02c83ec9d3813d663c289dae06d1241234b583e
Christopher Pitt <cgpitt@gmail.com>
2020-01-06 10:30:53 +0200

    Move footer links into header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,7 +15,7 @@
         <div class="container max-w-4xl mx-auto px-2 text-gray-700 text-lg font-light">
             <nav
                 class="
-                    flex flex-row items-center justify-start px-4
+                    flex flex-row items-center justify-between px-4
                     @if(request()->is('/'))
                         mb-8
                     @else
@@ -23,17 +23,29 @@
                     @endif
                 "
             >
-                @if(request()->is('/'))
-                    <h1 class="flex text-3xl">Christopher Pitt</h1>
-                @else
-                    <span class="flex text-2xl">Christopher Pitt</span>
-                @endif
-                @if(!request()->is('/'))
-                    <a href="{{ route('show-home') }}" class="flex text-md text-blue-500 underline ml-2">Home</a>
-                @endif
+                <div class="flex flex-row items-center justify-start">
+                    @if(request()->is('/'))
+                        <h1 class="flex text-3xl">Christopher Pitt</h1>
+                    @else
+                        <span class="flex text-2xl">Christopher Pitt</span>
+                    @endif
+                    @if(!request()->is('/'))
+                        <a href="{{ route('show-home') }}" class="flex text-md text-blue-500 underline ml-2">Home</a>
+                    @endif
+                </div>
+                <div class="flex flex-row items-center justify-start">
+                    <a href="https://twitter.com/assertchris" class="inline-flex w-4 h-4 text-blue-400">
+                        @icon('brands.twitter')
+                    </a>
+                    <a href="https://github.com/assertchris" class="inline-flex w-4 h-4 text-blue-400 ml-2">
+                        @icon('brands.github')
+                    </a>
+                    <a href="{{ route('feeds.main') }}" class="inline-flex w-4 h-4 text-blue-400 ml-2">
+                        @icon('solid.rss')
+                    </a>
+                </div>
             </nav>
             @yield('content')
-            @include('includes.footer')
             <script src="{{ mix('js/app.js') }}"></script>
             @stack('scripts')
         </div>
