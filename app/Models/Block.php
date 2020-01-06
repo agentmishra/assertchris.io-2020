04b2a04c1d402a0dd4230c80ff92003924a9285e
Christopher Pitt <cgpitt@gmail.com>
2020-01-06 21:17:02 +0200

    Cache block markdown

--- app/Models/Block.php
+++ app/Models/Block.php
@@ -34,34 +34,40 @@ class Block extends Model
 
     public function getTextContentMarkdownAttribute()
     {
-        return Markdown::convertToHtml($this->text_content);
+        return cache()->remember('block-text-content-markdown-' . $this->id, now()->addMinutes(60), function () {
+            return Markdown::convertToHtml($this->text_content);
+        });
     }
 
     public function getNoteContentMarkdownAttribute()
     {
-        return Markdown::convertToHtml($this->note_content);
+        return cache()->remember('block-note-content-markdown-' . $this->id, now()->addMinutes(60), function () {
+            return Markdown::convertToHtml($this->note_content);
+        });
     }
 
     public function getCodeContentMarkdownAttribute()
     {
-        preg_match("/^```([^\\n]+)/", $this->code_content, $matches);
+        return cache()->remember('block-code-content-markdown-' . $this->id, now()->addMinutes(60), function () {
+            preg_match("/^```([^\\n]+)/", $this->code_content, $matches);
 
-        if (count($matches) < 2) {
-            return Markdown::convertToHtml($this->code_content);
-        }
+            if (count($matches) < 2) {
+                return Markdown::convertToHtml($this->code_content);
+            }
 
-        $lines = explode("\n", $this->code_content);
-        array_shift($lines);
-        array_pop($lines);
-        $code = implode("\n", $lines);
+            $lines = explode("\n", $this->code_content);
+            array_shift($lines);
+            array_pop($lines);
+            $code = implode("\n", $lines);
 
-        if (!static::$highlighter) {
-            static::$highlighter = new Highlighter();
-        }
+            if (!static::$highlighter) {
+                static::$highlighter = new Highlighter();
+            }
 
-        $highlighted = static::$highlighter->highlight($matches[1], $code);
+            $highlighted = static::$highlighter->highlight($matches[1], $code);
 
-        return "<pre><code class='hljs {$highlighted->language}'>{$highlighted->value}</code></pre>";
+            return "<pre><code class='hljs {$highlighted->language}'>{$highlighted->value}</code></pre>";
+        });
     }
 
     public function getImageUrlAttribute()
